3d4ff77ac19ae75c56f8ee0151d6f9074aa76b58
Simran Preet Singh <simran5590@gmail.com>
2022-06-13 07:24:29 -0700

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Simran Preet Narang on 2022-06-13.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
